336ed34abd8b2f250a6ad42ec42e2265b6aaf792
kurikinton105 <yamadakenta0101@gmail.com>
2020-05-31 14:34:27 +0900

    update code
    完成版１
    基本的な機能（書き込み）が終了

--- script.js
+++ script.js
@@ -13,12 +13,13 @@ document.getElementById("text-button").onclick = function() {
   document.getElementById("text").innerHTML = "クリックされた！";
 };
 
-function OnButtonClick(idname) {
+function OnButtonClick(idname,name) {
   var count = 0;
   target = document.getElementById("output");
-  var getresult = document.getElementById(idname).value;
-  target.innerHTML = "打てました！！"+ "π " + count+  " "+ getresult;
-  document.sample.value = getresult;
-  //document.form_003.textBox_003.value = text;
-  count += 1;
+
+  var getresult = document.getElementById(idname).value; //値の取得
+  var text = getresult + "  "+name +"  ";
+  //target.innerHTML = text;
+  inputform.input_txt.value=text; //フォームに表示
+
 };
